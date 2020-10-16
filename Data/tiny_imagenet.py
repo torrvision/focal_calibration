416d78d978e6df9bf16dd91a5e9d3d68f17059a7
Jishnu Mukhoti <omegafragger@gmail.com>
2020-10-16 18:13:59 +0100

    Added data loaders for CIFAR10, CIFAR-100, SVHN and Tiny ImageNet

---
+++ Data/tiny_imagenet.py
@@ -0,0 +1,175 @@
+"""
+Create train, val, test iterators for Tiny ImageNet.
+Train set size: 100000
+Val set size: 10000
+Test set size: 10000
+Number of classes: 200
+Link: https://tiny-imagenet.herokuapp.com/
+"""
+
+import os
+import torch
+import numpy as numpy
+
+from torchvision import datasets
+from torchvision import transforms
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+
+from torch.utils.data.sampler import SubsetRandomSampler
+
+import os
+import glob
+from torch.utils.data import Dataset
+from PIL import Image
+
+EXTENSION = 'JPEG'
+NUM_IMAGES_PER_CLASS = 500
+CLASS_LIST_FILE = 'wnids.txt'
+VAL_ANNOTATION_FILE = 'val_annotations.txt'
+
+
+class TinyImageNet(Dataset):
+    """Tiny ImageNet data set available from `http://cs231n.stanford.edu/tiny-imagenet-200.zip`.
+    Parameters
+    ----------
+    root: string
+        Root directory including `train`, `test` and `val` subdirectories.
+    split: string
+        Indicating which split to return as a data set.
+        Valid option: [`train`, `test`, `val`]
+    transform: torchvision.transforms
+        A (series) of valid transformation(s).
+    in_memory: bool
+        Set to True if there is enough memory (about 5G) and want to minimize disk IO overhead.
+    """
+    def __init__(self, root, split='train', transform=None, target_transform=None, in_memory=False):
+        self.root = os.path.expanduser(root)
+        self.split = split
+        self.transform = transform
+        self.target_transform = target_transform
+        self.in_memory = in_memory
+        self.split_dir = os.path.join(root, self.split)
+        self.image_paths = sorted(glob.iglob(os.path.join(self.split_dir, '**', '*.%s' % EXTENSION), recursive=True))
+        self.labels = {}  # fname - label number mapping
+        self.images = []  # used for in-memory processing
+
+        # build class label - number mapping
+        with open(os.path.join(self.root, CLASS_LIST_FILE), 'r') as fp:
+            self.label_texts = sorted([text.strip() for text in fp.readlines()])
+        self.label_text_to_number = {text: i for i, text in enumerate(self.label_texts)}
+
+        if self.split == 'train':
+            for label_text, i in self.label_text_to_number.items():
+                for cnt in range(NUM_IMAGES_PER_CLASS):
+                    self.labels['%s_%d.%s' % (label_text, cnt, EXTENSION)] = i
+        elif self.split == 'val':
+            with open(os.path.join(self.split_dir, VAL_ANNOTATION_FILE), 'r') as fp:
+                for line in fp.readlines():
+                    terms = line.split('\t')
+                    file_name, label_text = terms[0], terms[1]
+                    self.labels[file_name] = self.label_text_to_number[label_text]
+
+        # read all images into torch tensor in memory to minimize disk IO overhead
+        if self.in_memory:
+            self.images = [self.read_image(path) for path in self.image_paths]
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, index):
+        file_path = self.image_paths[index]
+
+        if self.in_memory:
+            img = self.images[index]
+        else:
+            img = self.read_image(file_path)
+
+        if self.split == 'test':
+            return img
+        else:
+            # file_name = file_path.split('/')[-1]
+            return img, self.labels[os.path.basename(file_path)]
+
+    def __repr__(self):
+        fmt_str = 'Dataset ' + self.__class__.__name__ + '\n'
+        fmt_str += '    Number of datapoints: {}\n'.format(self.__len__())
+        tmp = self.split
+        fmt_str += '    Split: {}\n'.format(tmp)
+        fmt_str += '    Root Location: {}\n'.format(self.root)
+        tmp = '    Transforms (if any): '
+        fmt_str += '{0}{1}\n'.format(tmp, self.transform.__repr__().replace('\n', '\n' + ' ' * len(tmp)))
+        tmp = '    Target Transforms (if any): '
+        fmt_str += '{0}{1}'.format(tmp, self.target_transform.__repr__().replace('\n', '\n' + ' ' * len(tmp)))
+        return fmt_str
+
+    def read_image(self, path):
+        img = Image.open(path)
+        if (img.mode == 'L'):
+            img = img.convert('RGB')
+        return self.transform(img) if self.transform else img
+
+
+def get_data_loader(root,
+                    batch_size,
+                    split='train',
+                    shuffle=True,
+                    num_workers=4,
+                    pin_memory=False):
+    """
+    Utility function for loading and returning train and valid
+    multi-process iterators over the Tiny Imagenet dataset. A sample
+    9x9 grid of the images can be optionally displayed.
+    If using CUDA, num_workers should be set to 1 and pin_memory to True.
+    Params
+    ------
+    - root: The root directory for TinyImagenet dataset
+    - batch_size: how many samples per batch to load.
+    - split: Can be train/val/test. For train we apply the data augmentation techniques.
+    - shuffle: whether to shuffle the train/validation indices.
+    - num_workers: number of subprocesses to use when loading the dataset.
+    - pin_memory: whether to copy tensors into CUDA pinned memory. Set it to
+      True if using GPU.
+    Returns
+    -------
+    - train_loader: training set iterator.
+    - valid_loader: validation set iterator.
+    """
+
+    normalize = transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    )
+
+    val_test_transform = transforms.Compose([
+            transforms.ToTensor(),
+            normalize
+    ])
+
+    train_transform = transforms.Compose([
+            transforms.RandomCrop(64, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize
+    ])
+
+    # load the dataset
+    data_dir = root
+
+    if (split == 'train'):
+        dataset = TinyImageNet(data_dir,
+                               split='train',
+                               transform=train_transform,
+                               in_memory=True)
+    else:
+        dataset = TinyImageNet(data_dir,
+                               split='val',
+                               transform=val_test_transform,
+                               in_memory=True)
+
+    data_loader = torch.utils.data.DataLoader(
+        dataset, batch_size=batch_size,
+        num_workers=num_workers, pin_memory=pin_memory, shuffle=True
+    )
+
+    return data_loader
