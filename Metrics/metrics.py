fbc86967a51bf749a97f4345bb52f9e5354cbf88
Jishnu Mukhoti <omegafragger@gmail.com>
2020-10-22 10:44:23 +0100

    Added code for temperature scaling using cross validation on ECE and NLL

--- Metrics/metrics.py
+++ Metrics/metrics.py
@@ -8,8 +8,10 @@ References:
 
 import math
 import torch
-from torch.nn import functional as F
+import numpy as np
 from torch import nn
+from torch.nn import functional as F
+
 
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
@@ -136,3 +138,102 @@ def test_classification_net(model, data_loader, device):
 
     return confusion_matrix(labels_list, predictions_list), accuracy, labels_list,\
         predictions_list, confidence_vals_list
+
+
+# Calibration error scores in the form of loss metrics
+class ECELoss(nn.Module):
+    '''
+    Compute ECE (Expected Calibration Error)
+    '''
+    def __init__(self, n_bins=15):
+        super(_ECELoss, self).__init__()
+        bin_boundaries = torch.linspace(0, 1, n_bins + 1)
+        self.bin_lowers = bin_boundaries[:-1]
+        self.bin_uppers = bin_boundaries[1:]
+
+    def forward(self, logits, labels):
+        softmaxes = F.softmax(logits, dim=1)
+        confidences, predictions = torch.max(softmaxes, 1)
+        accuracies = predictions.eq(labels)
+
+        ece = torch.zeros(1, device=logits.device)
+        for bin_lower, bin_upper in zip(self.bin_lowers, self.bin_uppers):
+            # Calculated |confidence - accuracy| in each bin
+            in_bin = confidences.gt(bin_lower.item()) * confidences.le(bin_upper.item())
+            prop_in_bin = in_bin.float().mean()
+            if prop_in_bin.item() > 0:
+                accuracy_in_bin = accuracies[in_bin].float().mean()
+                avg_confidence_in_bin = confidences[in_bin].mean()
+                ece += torch.abs(avg_confidence_in_bin - accuracy_in_bin) * prop_in_bin
+
+        return ece
+
+
+class AdaptiveECELoss(nn.Module):
+    '''
+    Compute Adaptive ECE
+    '''
+    def __init__(self, n_bins=15):
+        super(_AdaptiveECELoss, self).__init__()
+        self.nbins = n_bins
+
+    def histedges_equalN(self, x):
+        npt = len(x)
+        return np.interp(np.linspace(0, npt, self.nbins + 1),
+                     np.arange(npt),
+                     np.sort(x))
+    def forward(self, logits, labels):
+        softmaxes = F.softmax(logits, dim=1)
+        confidences, predictions = torch.max(softmaxes, 1)
+        accuracies = predictions.eq(labels)
+        n, bin_boundaries = np.histogram(confidences.cpu().detach(), self.histedges_equalN(confidences.cpu().detach()))
+        #print(n,confidences,bin_boundaries)
+        self.bin_lowers = bin_boundaries[:-1]
+        self.bin_uppers = bin_boundaries[1:]
+        ece = torch.zeros(1, device=logits.device)
+        for bin_lower, bin_upper in zip(self.bin_lowers, self.bin_uppers):
+            # Calculated |confidence - accuracy| in each bin
+            in_bin = confidences.gt(bin_lower.item()) * confidences.le(bin_upper.item())
+            prop_in_bin = in_bin.float().mean()
+            if prop_in_bin.item() > 0:
+                accuracy_in_bin = accuracies[in_bin].float().mean()
+                avg_confidence_in_bin = confidences[in_bin].mean()
+                ece += torch.abs(avg_confidence_in_bin - accuracy_in_bin) * prop_in_bin
+        return ece
+
+
+class ClasswiseECELoss(nn.Module):
+    '''
+    Compute Classwise ECE
+    '''
+    def __init__(self, n_bins=15):
+        super(_ClasswiseECELoss, self).__init__()
+        bin_boundaries = torch.linspace(0, 1, n_bins + 1)
+        self.bin_lowers = bin_boundaries[:-1]
+        self.bin_uppers = bin_boundaries[1:]
+
+    def forward(self, logits, labels):
+        num_classes = int((torch.max(labels) + 1).item())
+        softmaxes = F.softmax(logits, dim=1)
+        per_class_sce = None
+
+        for i in range(num_classes):
+            class_confidences = softmaxes[:, i]
+            class_sce = torch.zeros(1, device=logits.device)
+            labels_in_class = labels.eq(i) # one-hot vector of all positions where the label belongs to the class i
+
+            for bin_lower, bin_upper in zip(self.bin_lowers, self.bin_uppers):
+                in_bin = class_confidences.gt(bin_lower.item()) * class_confidences.le(bin_upper.item())
+                prop_in_bin = in_bin.float().mean()
+                if prop_in_bin.item() > 0:
+                    accuracy_in_bin = labels_in_class[in_bin].float().mean()
+                    avg_confidence_in_bin = class_confidences[in_bin].mean()
+                    class_sce += torch.abs(avg_confidence_in_bin - accuracy_in_bin) * prop_in_bin
+
+            if (i == 0):
+                per_class_sce = class_sce
+            else:
+                per_class_sce = torch.cat((per_class_sce, class_sce), dim=0)
+
+        sce = torch.mean(per_class_sce)
+        return sce
